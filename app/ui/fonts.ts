263d0947f40cf79424105eefd2bcb0ed2601310e
NAING <NaingNaingTun@gginipponlife.com>
2025-07-12 23:47:30 +0630

    font issue fix

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// app/ui/fonts.ts
+// Add your font imports and exports here. Example with next/font/local or next/font/google:
+
+// Example using next/font/google
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+// Placeholder export to prevent build errors
+//export const lusitana = {};
